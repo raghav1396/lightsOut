134ab0d053c01abdbaec40d9a951ae967470c1b3
Raghav Bhardwaj <raghavbhardwaj32@gmail.com>
2021-07-26 01:38:06 +0530

    Updates

---
+++ precache-manifest.a29b99dd91c21726c22e57c94ed88ba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f889416bcd41bd2263de",
+    "url": "/lightsOut/static/css/main.da5e1cc4.chunk.css"
+  },
+  {
+    "revision": "f889416bcd41bd2263de",
+    "url": "/lightsOut/static/js/main.f889416b.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/lightsOut/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "7bb51e0662d9c55e23aa",
+    "url": "/lightsOut/static/js/runtime~main.7bb51e06.js"
+  },
+  {
+    "revision": "613a505839e670279bdd092065137285",
+    "url": "/lightsOut/index.html"
+  }
+];
